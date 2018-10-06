280fe5af65fa7ff5d93db35a3ac342d022b2b754
daniobiols <danielaobiols@gmail.com>
2018-10-06 09:19:19 -0300

    06.10.2018

--- classes/DBMySQL.php
+++ classes/DBMySQL.php
@@ -45,44 +45,44 @@ class DBMySQL extends DB
 		}
 	}
 
-	public function searchEmail($email)
-	{
-		$opt     = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-		$host    = 'mysql:host=127.0.0.1;dbname=FID;port=8889';
-		$db_user = 'root';
-		$db_pass = 'root';
-
-		$columnas = '';
-		$values = '';
-
-		foreach ($datos as $key => $value)
-		{
-			if (in_array($key, $model->columns))
-			{
-				$columnas .= $key . ',';
-				$values .= '"' . $value . '",';
-			}
-		}
-
-		$columnas = trim($columnas, ',');
-		$values = trim($values, ',');
-
-		try {
-			$db = new PDO($host, $db_user, $db_pass, $opt);
-			$sql = 'SELECT ('.$values.')' FROM '.$model->table.' ('.$columnas.');
-
-			$query = $db->prepare($sql);
-			$query->execute();
-			$db = null;
-
-		} catch(\Exception $e) {
-
-			var_dump($e);
-
-			// ENTRO SOLO SI ERROR
-			echo $e->getMessage();
-		}
-	}
+	 public function searchEmail($email)
+	 {
+	// 	$opt     = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+	// 	$host    = 'mysql:host=127.0.0.1;dbname=FID;port=8889';
+	// 	$db_user = 'root';
+	// 	$db_pass = 'root';
+	//
+	// 	$columnas = '';
+	// 	$values = '';
+	//
+	// 	foreach ($datos as $key => $value)
+	// 	{
+	// 		if (in_array($key, $model->columns))
+	// 		{
+	// 			$columnas .= $key . ',';
+	// 			$values .= '"' . $value . '",';
+	// 		}
+	// 	}
+	//
+	// 	$columnas = trim($columnas, ',');
+	// 	$values = trim($values, ',');
+	//
+	// 	try {
+	// 		$db = new PDO($host, $db_user, $db_pass, $opt);
+	// 		$sql = 'SELECT ('.$values.')' FROM '.$model->table.' ('.$columnas.');
+	//
+	// 		$query = $db->prepare($sql);
+	// 		$query->execute();
+	// 		$db = null;
+	//
+	// 	} catch(\Exception $e) {
+	//
+	// 		var_dump($e);
+	//
+	// 		// ENTRO SOLO SI ERROR
+	// 		echo $e->getMessage();
+	// 	}
+	 }
 
 	public function loadAll()
 	{
